cf6ccf5118694cf11c4cd729b7dbbc4c6da08781
nithigithub <nithiyadevi2001@gmail.com>
2021-03-14 19:24:04 +0530

    indexpage

--- aiclselectform.php
+++ aiclselectform.php
@@ -19,11 +19,7 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
     echo "<table border='1'>";
     echo"<tr><th>Firstname</th><tr>Lastname</tr><tr>Email</tr><th>Contact</th><th>Message</th></tr>";
-    echo "<tr><td>" . $row['firstname'] . "</td>";
-    echo "<tr><td>" . $row['lastname'] . "</td>";
-    echo "<tr><td>" . $row['Email'] . "</td>";
-    echo "<td>" . $row['contact'] . "</td>";
-    echo "<td>" . $row['messagetext'] . "</td></tr>";
+    echo "<tr><td>" . $row['firstname']  . $row['lastname']  . $row['Email'] . $row['contact'] . $row['messagetext'] . "</td></tr>";
     echo "</table>";
   }
 } else {
