25a0432e72e513af81967c14fd1c99f551b6e643
nithigithub <nithiyadevi2001@gmail.com>
2021-03-14 22:19:48 +0530

    indexpages

--- aiclselectform.php
+++ aiclselectform.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "myDB";
+$dbname = "aiclstudents";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,19 +11,15 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM AiclTables";
+$sql = "SELECT * FROM Aicldetail";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 
   while($row = $result->fetch_assoc()) {
     echo "<table border='1'>";
-    echo"<tr><th>Firstname</th><th>Lastname</th><th>Email</th><th>Contact</th><th>Message</th></tr>";
-    echo "<tr><td>" . $row['firstname'] . "</td>";
-    echo "<td>" .$row['lastname'] . "</td>";
-    echo "<td>" .$row['email'] . "</td>" ;
-    echo "<td>" . $row['contact'] . "</td>";
-    echo "<td>" . $row['messages'] . "</td></tr>";
+    echo"<tr><th>firstname</th><th>lastname</th><th>contact</th><th>email</th><th>message</th></tr>";
+    echo "<tr><td>" . $row['firstname'] .$row['lastname'] . $row['contact'] . $row['email'] . $row['messages'] . "</td></tr>";
     echo "</table>";
   }
 } else {
